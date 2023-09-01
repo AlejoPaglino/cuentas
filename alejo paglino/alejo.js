8dc1518efa5ad0f67f598107b1ed9f799677315a
AlejoPaglino <136344335+AlejoPaglino@users.noreply.github.com>
2023-09-01 08:56:46 -0300

    Add files via upload

---
+++ alejo paglino/alejo.js
@@ -0,0 +1,60 @@
+function cubo(b)
+{
+    let resultrado = b * b * b
+    return resultado
+}
+alert(cubo(2))
+function suma(a, b)
+{let multiplicacion =  (a * b)
+    return ' el resultado es' + suma
+}
+{let division = (a / b) 
+    return 'el  resultado es' + division}
+
+{let suma = (a + b)
+return 'el  resultado es' + suma}
+let resta =(a - b)
+function cuadrado(num)
+ {
+ return num *num
+}
+ function cubo ( num)
+ {
+     return num * num * num
+ }
+ //a) x^2-x-10
+ function operacionA(x)
+ {
+     let resultado = (x*x) - x -10
+     return resultado
+ }
+// b) (x-3)*2
+function operacionB(x)
+ {
+     let resultado = (x-3) *2
+     return resultado
+ }
+ //c) x*x^2+20
+ function operacionC(x)
+ {
+     let resultado =(x*x*x)+20
+     return resultado
+ }
+//d) (x/2)+5
+function operacionD(x)
+ {
+let resultado=(x/2)+5
+return resultado
+ }
+ //e) x^3+x^2+10
+ function operacionE(x)
+ {
+     let resultado=(x * x * x) + (x * x) + 10
+     return resultado
+ }
+ //f) (x*10)*5
+ function operacionF(x)
+ {
+     let resultado=(x*10)*5
+ return resultado
+}
